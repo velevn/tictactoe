ab969b54fe8d5227257dcf2b908399e87fbf4125
Nikita <velevnik@gmail.com>
2023-10-04 07:09:05 +0400

    Новая версия

---
+++ main.py
@@ -0,0 +1,27 @@
+BOARD_SIZE = 3
+BOARD = [1, 2, 3, 4, 5, 6, 7, 8,]
+
+# функция отрисовки поля
+def draw():
+    print(' '+'-'*15)
+
+
+# функция хода игрока
+def step_game():
+    pass
+
+# проверка победной комбинации
+def win_check():
+    pass
+
+# запуск игры
+def start_game():
+    draw()
+
+
+def main():
+    start_game()
+
+
+if __name__ == '__main__':
+    main()
